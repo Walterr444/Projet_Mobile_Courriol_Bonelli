8ab5367c70c2361961a5e5a371e5b1617c3ffc5d
Florian Bonelli <florian.bonelliduquesnoy@edu.ece.fr>
2026-02-16 12:33:44 +0100

    server

---
+++ server/pb_hooks/main.pb.js
@@ -0,0 +1,15 @@
+routerAdd("GET", "/hello/{name}", (e) => {
+    let name = e.request.pathValue("name")
+
+    return e.json(200, { "message": "Hello " + name })
+})
+
+onRecordAfterUpdateSuccess((e) => {
+    console.log("user updated...", e.record.get("email"))
+
+    e.next()
+}, "users")
+
+cronAdd("hello", "*/2 * * * *", () => {
+    console.log("Hello!")
+})
